b403a83f3ad90cd77398b2974c4e05fa20224eec
Saqib861 <106102177+Saqib861@users.noreply.github.com>
2022-09-21 21:42:05 +0530

    Create Print string using pointer.c

---
+++ Print string using pointer.c
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+
+ int main() 
+
+ { 
+
+   char name[]="Faisal"; 
+
+   char *p; 
+
+   p=&name[0]; 
+
+   while(*p!='\0') 
+
+   { 
+
+           printf("%c",*p); 
+
+           p++; 
+
+   } 
+
+ } 
+
+   
