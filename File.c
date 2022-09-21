573c499f0658368444099a14ed38907c0fa1f844
Saqib861 <106102177+Saqib861@users.noreply.github.com>
2022-09-21 21:33:51 +0530

    Create File.c

---
+++ File.c
@@ -0,0 +1,21 @@
+//file  
+
+ #include<stdio.h> 
+
+ int main() 
+
+ { 
+
+         char s[20]="faisal"; 
+
+         int x=6; 
+
+         FILE *p; 
+
+         p=fopen("file1.txt","w"); 
+
+         fprintf(p,"%d%s",x,s); 
+
+         fclose(p); 
+
+ }
