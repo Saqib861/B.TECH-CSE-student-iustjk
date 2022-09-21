bc288b4cfeb8b260a1861698c4a16841f729c9a6
Saqib861 <106102177+Saqib861@users.noreply.github.com>
2022-09-21 21:31:19 +0530

    Create Pyramid.c

---
+++ Pyramid.c
@@ -0,0 +1,27 @@
+#include<stdio.h> 
+
+ int main() 
+
+ { 
+
+         int i,n; 
+
+         for(i=1;i<=6;i++) 
+
+              { 
+
+                      for(n=1;n<=i;n++) 
+
+                      { 
+
+                              printf("*  "); 
+
+                      } 
+
+                printf("\n"); 
+
+              } 
+
+              return 0; 
+
+ }
