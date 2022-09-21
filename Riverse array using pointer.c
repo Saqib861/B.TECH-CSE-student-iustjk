dc705c9c33632c8bb576bf0e9dfcad9e7194df3d
Saqib861 <106102177+Saqib861@users.noreply.github.com>
2022-09-21 21:41:11 +0530

    Create Riverse array using pointer.c

---
+++ Riverse array using pointer.c
@@ -0,0 +1,33 @@
+#include<stdio.h> 
+
+ int main() 
+
+ { 
+
+         char *p="Faisal rashid"; 
+
+         int l=0,i; 
+
+         while(*p!='\0') 
+
+         { 
+
+                 l++; 
+
+                 p++; 
+
+         } 
+
+  p--; 
+
+ for(i=l-1;i>=0;i--) 
+
+ { 
+
+  printf("%c",*p); 
+
+  p--; 
+
+ } 
+
+ }
