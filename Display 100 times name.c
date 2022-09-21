85b6c66133a372334512020ae5671f5a2b7d9725
Saqib861 <106102177+Saqib861@users.noreply.github.com>
2022-09-21 21:28:55 +0530

    Create Display 100 times name.c

---
+++ Display 100 times name.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+
+ int main() 
+
+ { 
+
+         int i; 
+
+         for(i=1;i<=100;i++) 
+
+         { 
+
+                 printf("Faisal Rashid\t"); 
+
+         } 
+
+         return 0; 
+
+ }
